08ad52a9bd0197156b06885ff501abc14d9bd262
Weiyuan Lyu <lyuweiyu@weiyuans-MacBook-Pro.local>
2023-04-02 23:47:15 -0400

    upload

---
+++ js/video.js
@@ -0,0 +1,67 @@
+var video;
+video = document.querySelector("#player1");
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window");
+	video.autoplay = false;
+	video.loop = false;
+});
+
+document.querySelector("#play").addEventListener(
+	"click",function() {
+		video.play();
+		document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
+	}
+);
+
+document.querySelector("#pause").addEventListener(
+	"click",function() {
+		video.pause();
+	}
+);
+
+document.querySelector("#slower").addEventListener("click", function () {
+	video.playbackrate -= 0.1;
+	console.log("New speed is ${video.playbackrate}")
+});
+
+document.querySelector("#faster").addEventListener("click", function (){
+	video.playbackrate += 0.1;
+	console.log("New speed is ${video.playbackrate}")
+});
+
+document.querySelector("#skip").addEventListener("click", function (){
+	video.currentTime += 10;
+	if(video.ended == true){
+		video.currentTime = 0;
+	}
+	console.log(video.currentTime)
+});
+
+document.querySelector("#mute").addEventListener("click", function (){
+	let muteText = document.querySelector("#mute");
+	if(video.muted){
+		video.muted = false;
+		muteText.innerHTML = "Unmute";
+	}else{
+		video.muted = true;
+		muteText.innerHTML = "Mute";
+	}
+});
+
+document.querySelector("slider").addEventListener("change",function (){
+	video.volume = parseFloat(this.value) / 100;
+	document.querySelector("#volume").innerHTML = this.value  + "%";
+
+})
+
+document.querySelector("#vintage").addEventListener("click",function() {
+	video.classList.add("oldSchool");
+});
+document.querySelector("#orig").addEventListener("click", function (){
+	video.classList.remove("oldSchool");
+})
+// document.querySelector("#play").addEventListener("click", function() {
+// 	console.log("Play Video");
+// });
+
